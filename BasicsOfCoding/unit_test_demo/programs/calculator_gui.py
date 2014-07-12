f5e0d181feefb687fa9fea3fcac489491b15ea1b
Lay Kuan Loh <lkloh2410@gmail.com>
2014-07-11 23:09:59 -0400

    added

--- BasicsOfCoding/unit_test_demo/programs/calculator_gui.py
+++ BasicsOfCoding/unit_test_demo/programs/calculator_gui.py
@@ -12,6 +12,15 @@ class CalculatorGUI:
 	def setup_gui(self):
 		fig = py.figure('Calculator')
 
+		xx = 0.4
+		yy = 0.1
+		xm = 0.1
+		diff = 0.15
+
+		axs = {}
+		axs['+'] = fig.add_axes([xm, diff*1, xx, yy])
+		axs['-'] = fig.add_axes([xm, diff*2, xx, yy])
+
 
 
 
