570fb03e33337079aa357694d28550dffd2c6f26
Lay Kuan Loh <lkloh2410@gmail.com>
2014-07-11 22:24:31 -0400

    added

--- BasicsOfCoding/unit_test_demo/test_simple_calculator.py
+++ BasicsOfCoding/unit_test_demo/test_simple_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from programs import simple_calculator 
+from programs.simple_calculator import add, subtract, times, divide 
 
 # ############################################################################### #
 #                                     MODELS                                      #
@@ -8,10 +8,10 @@ from programs import simple_calculator
 class calculatorModel(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(3,4), 3+47)
+        self.assertEqual(add(3,4), 3+4)
 
     def test_subtract(self):
-    	self.assertEqual(subtract(4,3), -1)
+    	self.assertEqual(subtract(4,3), 1)
 
     def test_multiply(self):
     	self.assertEqual(times(3,4), 12)
